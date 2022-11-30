96539a76fdb2b5da091f25a09cb06095f77c57bf
analogica <superanalogica@gmail.com>
2022-11-30 20:02:41 +0100

    creo el archivo que imprime Hola mundo

---
+++ Pro_InicialesNombreApellidos/src/examen/hola.java
@@ -0,0 +1,10 @@
+package examen;
+
+public class hola {
+
+	public static void main(String[] args) {
+		// EXAMEN ANALOGICA
+		System.out.println(" ¡Hola mundo soy “Mi nombre”!");
+	}
+
+}
---
+++ Pro_InicialesNombreApellidos/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author super
+ *
+ */
+module Pro_InicialesNombreApellidos {
+}
